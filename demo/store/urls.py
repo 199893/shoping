05cff431991335f094041dc8cf362b748333caab
jmx <695205458@qq.com>
2019-05-07 16:15:52 +0800

    模板

--- demo/store/urls.py
+++ demo/store/urls.py
@@ -5,4 +5,6 @@ app_name='store'
 urlpatterns = [
     url('^$',views.index,name='index'),
     url('^login/$',views.login,name='login'),
+    url('^signup/$',views.signup,name='signup'),
+    url(r'^offers/$',views.offers,name='offers'),
 ]
--- demo/store/views.py
+++ demo/store/views.py
@@ -3,8 +3,18 @@ from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 
+#首页
 def index(request):
     return render(request,'store/index.html')
 
+#登录
 def login(request):
     return render(request,'store/login.html')
+
+#注册
+def signup(reqyest):
+    return render(reqyest,'store/signup.html')
+
+#优惠界面
+def offers(request):
+    return render(request,'store/offers.html')
