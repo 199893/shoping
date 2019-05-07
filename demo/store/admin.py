0646c28c7cd466cdd2eae4c4adf031a2c01d87ed
skrlen <1213127713skrlen@users.noreply.github.com>
2019-05-07 16:19:38 +0800

    创建了数据库表，用户商品购物车等。。。

--- demo/store/admin.py
+++ demo/store/admin.py
@@ -1,3 +1,80 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class User_admin(admin.ModelAdmin):
+    """
+    用户管理界面
+    """
+    #默认展示
+    list_display = ['username','is_active','email']
+    #分类
+    list_filter = ['username','is_active']
+    #默认搜索
+    search_fields = ['username','email']
+    #默认分页
+    list_per_page = 10
+
+
+class Classification_admin(admin.ModelAdmin):
+    """
+    商品分类管理
+    """
+    # 默认展示
+    list_display = ['classname', 'date']
+    # 分类
+    list_filter = ['classname', 'date']
+    # 默认搜索
+    search_fields = ['classname']
+    # 默认分页
+    list_per_page = 10
+
+
+class Commodity_admin(admin.ModelAdmin):
+    """
+    商品详细分类管理
+    """
+    # 默认展示
+    list_display = ['cname', 'cclassification']
+    # 分类
+    list_filter = ['cname', 'cclassification']
+    # 默认搜索
+    search_fields = ['cname']
+    # 默认分页
+    list_per_page = 10
+
+class Moregoods_admin(admin.ModelAdmin):
+    """
+    商品细致分类表
+    """
+    # 默认展示
+    list_display = ['mcommodity', 'gcommodify']
+    # 分类
+    list_filter = ['mcommodity', 'gcommodify']
+    # 默认搜索
+    search_fields = ['mcommodity']
+    # 默认分页
+    list_per_page = 10
+
+
+class Goods_admin(admin.ModelAdmin):
+    """
+    商品管理
+    """
+    # 默认展示
+    list_display = ['goodsname', 'price','gmoregoods']
+    # 分类
+    list_filter = ['goodsname', 'gmoregoods']
+    # 默认搜索
+    search_fields = ['goodsname','price']
+    # 默认分页
+    list_per_page = 10
+
+
+admin.site.register(User,User_admin)
+admin.site.register(Moregoods,Moregoods_admin)
+admin.site.register(Classification,Classification_admin)
+admin.site.register(Commodity,Commodity_admin)
+admin.site.register(Goods,Goods_admin)
+
--- demo/store/models.py
+++ demo/store/models.py
@@ -1,3 +1,96 @@
 from django.db import models
 
 # Create your models here.
+
+
+#用户表
+class User(models.Model):
+    """
+    用户表，可以对用户的信息进行保存
+    """
+    #用户名
+    username = models.CharField(max_length=20)
+    #密码
+    password = models.CharField(max_length=100)
+    #邮件
+    email = models.EmailField()
+    # 金钱
+    money = models.IntegerField(default=0)
+    #激活
+    is_active= models.BooleanField(default=False)
+    #注册时间
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+#商品大分类表
+class Classification(models.Model):
+    """
+    商品大分类表，可以对细分为多个商品分类
+    """
+    #商品分类名称
+    classname = models.CharField(max_length=30)
+    #分类创建时间
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.classname
+
+class Commodity(models.Model):
+    """
+    商品中分表，详细的分类出每个大分类的详细分类
+    """
+    #详细分类名称
+    cname = models.CharField(max_length=30)
+    #所属哪个分类
+    cclassification = models.ForeignKey(Classification,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.cname
+
+
+class Moregoods(models.Model):
+    """
+    所有分类，商品细分表，详细的分类出每个分类的详细分类
+    """
+    #分类名
+    mcommodity = models.CharField(max_length=30)
+    #分类数据外键
+    gcommodify = models.ForeignKey(Commodity, on_delete=models.CASCADE)
+    #时间
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.mcommodity
+
+class Goods(models.Model):
+    """
+    商品表，详细的商品信息
+    """
+    #商品名称
+    goodsname = models.CharField(max_length=30)
+    #商品价格
+    price = models.IntegerField(default=0)
+    #商品折扣
+    discount = models.FloatField(default=1)
+    # 商品增加时间
+    date = models.DateTimeField(auto_now_add=True)
+    #所属分类
+    gmoregoods = models.ForeignKey(Moregoods,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.goodsname
+
+
+class Shoppingcart(models.Model):
+    """
+    购物车
+    """
+    #用户id
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    #商品id
+    goods_id = models.ForeignKey(Goods,on_delete=models.CASCADE)
+    #添加时间
+    date = models.DateTimeField(auto_now_add=True)
+
