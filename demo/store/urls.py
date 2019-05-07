7f51f6ab9c2675ba8acfb20fdd356f9f9d9819df
taoxiaodi <1104056609@qq.com>
2019-05-07 17:04:11 +0800

    底部链接

--- demo/store/urls.py
+++ demo/store/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 app_name='store'
 urlpatterns = [
-    url('^$',views.index,name='index'),
-    url('^login/$',views.login,name='login'),
-    url('^signup/$',views.signup,name='signup'),
-    url(r'^offers/$',views.offers,name='offers'),
+    url('^$', views.index, name='index'),
+    url('^login/$', views.signup, name='signup'),
+    url(r'^offers/$', views.offers, name='offers'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^markerplace/$', views.marketplace, name="markerplace"),
+    url(r'^values/$', views.values, name='values'),
+    url(r'^privacy/$', views.privacy, name="privacy"),
 ]
--- demo/store/views.py
+++ demo/store/views.py
@@ -3,18 +3,42 @@ from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 
-#首页
+
+# 首页
 def index(request):
     return render(request,'store/index.html')
 
-#登录
+
+# 登录
 def login(request):
     return render(request,'store/login.html')
 
-#注册
+
+# 注册
 def signup(reqyest):
     return render(reqyest,'store/signup.html')
 
-#优惠界面
+
+# 优惠界面
 def offers(request):
     return render(request,'store/offers.html')
+
+
+# 关于我们
+def about(request):
+    return render(request, 'store/about.html')
+
+
+# 市井
+def marketplace(request):
+    return render(request, 'store/marketplace.html')
+
+
+# 核心价值
+def values(request):
+    return render(request, 'store/values.html')
+
+
+# 隐私政策
+def privacy(request):
+    return render(request, 'store/privacy.html')
