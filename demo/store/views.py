2130ed15d9471e03b654f20343fad7b80b2375a2
skrlen <1213127713skrlen@users.noreply.github.com>
2019-05-08 14:13:19 +0800

    去除硬编码完成

--- demo/store/views.py
+++ demo/store/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render,reverse,redirect
 from django.http import HttpResponse,HttpResponseRedirect
+from .models import User,Classification,Commodity,Moregoods,Goods,Shoppingcart
+#邮件发送模块
+from django.core.mail import send_mail
+from django.conf import settings
+#邮件加密模块
+from itsdangerous import TimedJSONWebSignatureSerializer as res,SignatureExpired
+
 
 # Create your views here.
 
@@ -11,12 +18,72 @@ def index(request):
 
 # 登录
 def login(request):
-    return render(request,'store/login.html')
+    if request.method=='GET':
+        return render(request,'store/login.html')
+    elif request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        try:
+            res= User.objects.filter(email=email)[0]
+            if password == res.password and res.is_active==True:
+                return HttpResponse('登录成功')
+            elif password == res.password and res.is_active==False:
+                return HttpResponse('账号未激活')
+        except:
+            return HttpResponse('账号不存在')
+
+
+        return HttpResponse('密码错误')
+
 
 
 # 注册
 def signup(request):
-    return render(request,'store/signup.html')
+    if request.method=='GET':
+        return render(request,'store/signup.html')
+    elif request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+        user=User()
+        try:
+            if len(User.objects.filter(email=email)):
+                return HttpResponse('邮箱已存在')
+            else:
+                if password==password2:
+                    user.username=name
+                    user.email=email
+                    user.password=password
+                    user.save()
+
+                    id = User.objects.get(email=email).id
+                    print(id)
+
+                    serutil = res(settings.SECRET_KEY, 50)
+                    resultid = serutil.dumps({'userid': id}).decode('utf-8')
+
+                    send_mail("您需要点这封邮件激活商城品台账号", "<a href='http://127.0.0.1:8000/store/active/%s'> 点击此处激活</a>"%(resultid,), settings.DEFAULT_FROM_EMAIL, [email])
+
+                    return render(request,'store/login.html')
+                else:
+                    return render(request,'store/signup.html')
+        except:
+            return HttpResponse('注册失败')
+
+
+#邮件加密
+def active(request,idstr):
+
+    deser=res(settings.SECRET_KEY,50)
+    try:
+        obj=deser.loads(idstr)
+        user=User.objects.get(pk=obj['userid'])
+        user.is_active=True
+        user.save()
+        return redirect(reverse('store:login'))
+    except SignatureExpired as e:
+        return HttpResponse('链接已失效')
 
 
 # 优惠界面
@@ -42,6 +109,8 @@ def values(request):
 # 隐私政策
 def privacy(request):
     return render(request, 'store/privacy.html')
+
+
 
 
 
